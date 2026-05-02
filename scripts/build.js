3b5fa415de461151f52935039994a7f4f99a0074
k33n26 <37938131+k33n26@users.noreply.github.com>
2026-05-02 08:32:22 +0300

    Create build.js

---
+++ scripts/build.js
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+const worker =
+  "https://okrutv.k33n26.workers.dev";
+
+const lines =
+  fs
+    .readFileSync(
+      "channels.txt",
+      "utf8"
+    )
+    .split("\n")
+    .map(
+      x => x.trim()
+    )
+    .filter(Boolean);
+
+let m3u =
+  "#EXTM3U\n";
+
+for (
+  const line of lines
+) {
+
+  const [
+    name,
+    id,
+    logo,
+    group
+  ] =
+    line.split("|");
+
+  m3u +=
+`#EXTINF:-1 tvg-name="${name}" tvg-logo="${logo}" group-title="${group}",${name}
+${worker}/${id}.m3u8?q=1080
+
+`;
+}
+
+fs.mkdirSync(
+  "playlist",
+  {
+    recursive: true
+  }
+);
+
+fs.writeFileSync(
+  "playlist/index.m3u",
+  m3u
+);
